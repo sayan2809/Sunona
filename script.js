470d92ccc2f5e2c3d6011e983607d367dd9c8477
sayan2809 <dassayan966@gmail.com>
2023-06-25 20:01:49 +0530

    add scroll reveal

--- script.js
+++ script.js
@@ -5,3 +5,25 @@ menu.onclick = () => {
   menu.classList.toggle("bx-x");
   navbar.classList.toggle("open");
 };
+
+ScrollReveal({
+  reset: true,
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+});
+
+ScrollReveal().reveal(".main-title, .vission, .mission, .our-team", {
+  delay: 500,
+  origin: "left",
+});
+
+ScrollReveal().reveal(".main-img, .vissionpic, .missionpic", {
+  delay: 600,
+  origin: "bottom",
+});
+
+ScrollReveal().reveal(".main-para, .book-now, .text-box1, .wrapper1", {
+  delay: 700,
+  origin: "right",
+});
