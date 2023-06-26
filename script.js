1eed1bbb0a3752809e7887a4e29346cae79f6997
Sayan Das <85397296+sayan2809@users.noreply.github.com>
2023-06-26 18:09:01 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -13,17 +13,33 @@ ScrollReveal({
   delay: 400,
 });
 
-ScrollReveal().reveal(".main-title, .vission, .mission, .our-team", {
-  delay: 500,
-  origin: "left",
-});
+ScrollReveal().reveal(
+  ".main-title, .vission, .mission, .our-team, .volunteers",
+  {
+    delay: 500,
+    origin: "left",
+  }
+);
 
 ScrollReveal().reveal(".main-img, .vissionpic, .missionpic", {
   delay: 600,
   origin: "bottom",
 });
 
-ScrollReveal().reveal(".main-para, .book-now, .text-box1, .wrapper1", {
+ScrollReveal().reveal(
+  ".main-para, .book-now, .text-box1, .wrapper1, .core-team",
+  {
+    delay: 700,
+    origin: "right",
+  }
+);
+
+ScrollReveal().reveal(".volunteers-members", {
+  delay: 500,
+  origin: "top",
+});
+
+ScrollReveal().reveal(".core-members, .volunteers-members1", {
   delay: 700,
-  origin: "right",
+  origin: "bottom",
 });
